2376c90b7e3915e9299830d91f4afe379528c993
Tyler Jones <tejones1102@gmail.com>
2020-09-09 21:00:03 -0700

    Changes Made For Deployment.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
+require('newrelic');
 const express = require('express');
 const path = require('path');
-const { createProxyMiddleware }= require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
-const PORT = 3030;
+const PORT = process.env.PORT || 3030;
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
